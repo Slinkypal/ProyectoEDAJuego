94e82d21a71f01f637229b1c8628c417a46aa6a6
alex005M <montaalex04@gmail.com>
2026-02-04 15:05:58 -0500

    game save actualizada

--- game/game_management/save_manager.py
+++ game/game_management/save_manager.py
@@ -1,18 +1,17 @@
 import csv
 
+
 def save_results(players, mst, total_cost):
     with open("resultado.csv", "w", newline="", encoding="utf8") as f:
         writer = csv.writer(f)
 
-        # Puntajes
         writer.writerow(["Jugador", "Puntaje"])
-        for p in players:
-            writer.writerow([p.name, p.score])
+        for player in players:
+            writer.writerow([player.name, player.score])
 
         writer.writerow([])
 
-        # Kruskal
-        writer.writerow(["KRUSKAL - Árbol de Expansión Mínima"])
+        writer.writerow(["KRUSKAL - Arbol de Expansión Minima"])
         writer.writerow(["Nodo A", "Nodo B", "Peso"])
         for u, v, w in mst:
             writer.writerow([u, v, w])
